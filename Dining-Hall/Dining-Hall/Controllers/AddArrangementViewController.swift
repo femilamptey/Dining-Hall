ad4d802cf81f23560c6ff27422df72e1e5ca6e30
Femi Lamptey <femi.lamptey@gmail.com>
2018-08-10 11:34:22 -0700

    Added methods to input records into database

--- Dining-Hall/Dining-Hall/Controllers/AddArrangementViewController.swift
+++ Dining-Hall/Dining-Hall/Controllers/AddArrangementViewController.swift
@@ -25,21 +25,11 @@ class AddArangementViewController: UIViewController, UIDocumentPickerDelegate {
             print("You gat access yo")
             path = url.absoluteString
             CFURLStopAccessingSecurityScopedResource(url as CFURL)
+            DataBaseManager.importCSV()
         } else {
             print("You dont gat access")
         }
         print("I'm working")
-    }
-
-    func process(contents: String) {
-        let lines = contents.split(separator: "\n")
-        let columns = lines.split(separator: ",", maxSplits: 99, omittingEmptySubsequences: false)
-        
-    }
-    
-    func processFile(path: String) throws {
-        let contents =  try String.init(contentsOf: URL.init(fileURLWithPath: path)); do {}
-        try process(contents: contents)
     }
 
 }
--- Dining-Hall/Dining-Hall/DatabaseManager.swift
+++ Dining-Hall/Dining-Hall/DatabaseManager.swift
@@ -50,13 +50,34 @@ class DataBaseManager {
         }
         
         if sqlite3_bind_text(statement, bindingPosition, table, -1, nil) != SQLITE_OK {
-            print("Error binding absent count")
+            print("Error binding seating position")
         }
         
         return statement
     }
     
-    static func addValue(lastName: String, middleName: String?, firstName: String, table: String, bindingPosition: Int32) {
+    static func bindToAbsenteeStatement(statement: OpaquePointer?, lastName: String, middleName: String?, firstName: String, table: String, date: NSDate, bindingPosition: Int32) -> OpaquePointer? {
+        
+        if sqlite3_bind_text(statement, bindingPosition, lastName, -1, nil) != SQLITE_OK {
+            print("Error binding last name")
+        }
+        
+        if sqlite3_bind_text(statement, bindingPosition, firstName, -1, nil) != SQLITE_OK {
+            print("Error binding first name")
+        }
+        
+        if sqlite3_bind_text(statement, bindingPosition, table, -1, nil) != SQLITE_OK {
+            print("Error binding absent count")
+        }
+        
+        if sqlite3_bind_text(statement, bindingPosition, date.description, -1, nil) != SQLITE_OK {
+            print("Error binding lateness date")
+        }
+        
+        return statement
+    }
+    
+    static func addStudentTableLocation(lastName: String, middleName: String?, firstName: String, table: String, bindingPosition: Int32) {
         var statement: OpaquePointer?
         let ArragementQuery = "INSERT INTO Arrangement (lastName, firstName, table), VALUES (?, ?, ?)"
         
@@ -70,5 +91,26 @@ class DataBaseManager {
             print("Student saved succesfully")
         }
     }
+    
+    static func addAbsentee(lastName: String, middleName: String?, firstName: String, table: String, date: NSDate, bindingPosition: Int32) {
+        var statement: OpaquePointer?
+        let AbsenteeQuery = "INSERT INTO Absentees (lastName, firstName, table, date), VALUES (?, ?, ?, ?)"
+        
+        if sqlite3_prepare(db, AbsenteeQuery, -1, &statement, nil) != SQLITE_OK {
+            print("Error preparing statement")
+        }
+        
+        statement = bindToAbsenteeStatement(statement: statement, lastName: lastName, middleName: middleName, firstName: firstName, table: table, date: date, bindingPosition: bindingPosition)
+        
+        if sqlite3_step(statement) == SQLITE_DONE {
+            print("Student saved succesfully")
+        }
+    }
+    
+    static func importCSV(path: URL) {
+        
+    }
+    
+    
     
 }
