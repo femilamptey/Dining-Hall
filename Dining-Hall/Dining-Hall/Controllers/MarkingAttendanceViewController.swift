b51421231ea2e61ba1deb606f79b1f26d0fe302b
Femi Lamptey <femi.lamptey@gmail.com>
2018-10-22 09:03:24 -0700

    Progress update

--- Dining-Hall/Dining-Hall/Controllers/MarkingAttendanceViewController.swift
+++ Dining-Hall/Dining-Hall/Controllers/MarkingAttendanceViewController.swift
@@ -9,14 +9,10 @@
 import UIKit
 
 class MarkingAttendanceViewController: UIViewController, UITableViewDataSource {
-   
-    
-    
+
     @IBOutlet weak var tablesTbl: UITableView!
     
     @IBOutlet weak var namesTbl: UITableView!
-    
-    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 99
@@ -24,14 +20,15 @@ class MarkingAttendanceViewController: UIViewController, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        var cell: UITableViewCell!
+        let cell: UITableViewCell!
         
         if tableView == tablesTbl {
-            cell = tableView.dequeueReusableCell(withIdentifier: "tableName")
-            cell.textLabel?.text = DatabaseManager.getAllTables()[indexPath.row]
-        } else if tableView == namesTbl {
-
+            cell = tableView.dequeueReusableCell(withIdentifier: "tableName", for: indexPath)
+            cell.textLabel!.text = DatabaseManager.getAllTables()[indexPath.row]
+        } else  {
+            cell = tableView.dequeueReusableCell(withIdentifier: "tableName", for: indexPath)
         }
+        
         return cell
     }
 }
